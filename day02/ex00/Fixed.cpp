a23427a862fecbc15508fef8fd1083fa34f05b9a
Anton Sviridov <asvirido@e1r6p1.unit.ua>
2018-03-26 18:55:40 +0300

    add day02ex00

---
+++ day02/ex00/Fixed.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : decimals(8) {
+	std::cout << "Default constructor called" << "\n";
+	this->rawBits = 0;
+}
+
+Fixed::Fixed( Fixed const & fixed) : decimals(8) {
+	std::cout << "Copy constructor called" << "\n";
+	*this = fixed;
+}
+
+Fixed::~Fixed( void ) {
+	std::cout << "Destructor called" << "\n";
+	return ;
+}
+
+Fixed & Fixed::operator=( Fixed const & fixed ) {
+	std::cout << "Assignation operator called" << "\n";
+	this->rawBits = fixed.getRawBits();
+}
+
+int 	Fixed::getRawBits( void ) {
+ 	std::cout << "getRawBits member function called" << "\n";
+	return ( this->rawBits );
+}
+
+void	Fixed::setRawBits( int rawBits ) {
+    std::cout << "setRawBits member function called" << "\n";
+	this->rawBits = rawBits;
+}
---
+++ day02/ex00/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asvirido <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/26 18:24:38 by asvirido          #+#    #+#             */
+/*   Updated: 2018/03/26 18:50:38 by asvirido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int rawBits;
+		int const decimals;
+	public:
+		Fixed( void ); /* canonical */
+		Fixed( Fixed const & f ); /* canonical */
+		~Fixed( void ); /* canonical */
+		/* gets */
+			int getRawBits( void );
+		/* sets */
+			void setRawBits( void );
+		/* operator */	
+		Fixed & operator=( Fixed const & f );
+
+};
+
+#endif
