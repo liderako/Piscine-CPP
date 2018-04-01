be3962f9b715833a1fb6cc753d865d120649f4b7
Anton Sviridov <asvirido@e1r6p5.unit.ua>
2018-04-01 19:06:57 +0300

    add day02ex01

--- Day02/ex01/Fixed.cpp
+++ Day02/ex01/Fixed.cpp
@@ -1,32 +1,52 @@
 #include "Fixed.hpp"
-
-Fixed::Fixed( void ) : decimals(8) {
+#include <cmath>
+Fixed::Fixed(void) : decimals(8) {
 	std::cout << "Default constructor called" << "\n";
 	this->rawBits = 0;
 }
 
-Fixed::Fixed( Fixed const & fixed) : decimals(8) {
+Fixed::Fixed(float value) : decimals(8) {
+	this->setRawBits(roundf(value * (float)(1 << this->decimals)));
+}
+
+Fixed::Fixed(int value) : decimals(8) {
+	this->rawBits = value << this->decimals;
+}
+
+Fixed::Fixed(Fixed const & fixed) : decimals(8) {
 	std::cout << "Copy constructor called" << "\n";
 	*this = fixed;
 }
 
-Fixed::~Fixed( void ) {
+Fixed::~Fixed(void) {
 	std::cout << "Destructor called" << "\n";
 	return ;
 }
 
-Fixed & Fixed::operator=( Fixed const & fixed ) {
+float 	Fixed::toFloat(void) const {
+	return ((float)this->rawBits / (float)(1 << this->decimals));
+}
+
+int 	Fixed::toInt(void) const {
+	return (this->rawBits >> this->decimals);
+}
+
+Fixed & Fixed::operator=(Fixed const & fixed) {
 	std::cout << "Assignation operator called" << "\n";
 	this->rawBits = fixed.getRawBits();
 	return *this;
 }
 
-int 	Fixed::getRawBits( void ) const {
+int 	Fixed::getRawBits(void) const {
  	std::cout << "getRawBits member function called" << "\n";
 	return ( this->rawBits );
 }
 
-void	Fixed::setRawBits( int rawBits ) {
+void	Fixed::setRawBits(int rawBits) {
     std::cout << "setRawBits member function called" << "\n";
 	this->rawBits = rawBits;
+}
+
+std::ostream & operator<<(std::ostream & o, Fixed const & b) {
+	return (o << b.toFloat());
 }
--- Day02/ex01/Fixed.hpp
+++ Day02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: asvirido <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 18:24:38 by asvirido          #+#    #+#             */
-/*   Updated: 2018/03/27 19:34:58 by asvirido         ###   ########.fr       */
+/*   Updated: 2018/04/01 18:51:05 by asvirido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,23 @@ class Fixed {
 		int rawBits;
 		int const decimals;
 	public:
-		Fixed( void ); /* canonical */
-		Fixed( Fixed const & f ); /* canonical */
-		~Fixed( void ); /* canonical */
+		Fixed(void); /* canonical */
+		Fixed(Fixed const & f); /* canonical */
+		~Fixed(void); /* canonical */
+		Fixed(int x);
+		Fixed(float x);
+		
+		float 	toFloat(void) const;
+		int		toInt(void) const;
 		/* gets */
-			int getRawBits( void ) const ;
-		/* sets */
-			void setRawBits( int r );
-		/* operator */	
-		Fixed & operator=( Fixed const & f );
+		int getRawBits(void) const;
 
+		/* sets */
+		void setRawBits( int r );
+		/* operator */
+		Fixed & operator=(Fixed const & f);
 };
+
+std::ostream & operator<<(std::ostream & o, Fixed const & b);
 
 #endif
